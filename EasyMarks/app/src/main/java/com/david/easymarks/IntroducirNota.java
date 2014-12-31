80aba97e54997d5520605dff1c5d483a7ff3f8d9
David Lozano Campillo <lozanocampillo2000@gmail.com>
2014-12-31 19:05:44 +0100

    Creación de IntroducirNotas

--- EasyMarks/app/src/main/java/com/david/easymarks/IntroducirNota.java
+++ EasyMarks/app/src/main/java/com/david/easymarks/IntroducirNota.java
@@ -2,9 +2,11 @@ package com.david.easymarks;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -17,6 +19,11 @@ public class IntroducirNota extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_introducir_nota);
+
+        final EditText etNota = (EditText) findViewById(R.id.etNota);
+        final EditText etFecha = (EditText) findViewById(R.id.etFecha);
+
+        Button btIntroducirNotaAceptar = (Button) findViewById(R.id.btIntroducirNotaAceptar);
 
         Spinner spinner = (Spinner) findViewById(R.id.spAsignatura);
 
@@ -33,6 +40,19 @@ public class IntroducirNota extends Activity {
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
 
+            }
+        });
+
+        btIntroducirNotaAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(etNota.getText().toString().equals("") || etFecha.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "Rellena todas las casillas", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    //TODO Base de Datos
+                    Toast.makeText(getApplicationContext(), "Nota introducida correctamente", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
